e5d9304cea84d529ab3df6b92d54a0c79fdfe21c
Arun <arunpandiyan.uae@gmail.com>
2021-05-06 19:22:31 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -3,6 +3,7 @@ if hungry == "yes":
     print("Eat Samosa")
     print("Eat Pizza")
     print("Eat Burger")
+    print("Eat Fries")
 else:
     thirsty = input("Are you thirsty")
     if thirsty == "yes":
